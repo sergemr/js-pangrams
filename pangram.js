a9a4e18fe2365f1f200a05f748428474fecbfdd8
samor737 <sergio@appartisans.it>
2021-12-03 13:35:22 -0800

    refactor : code eficiency

--- pangram.js
+++ pangram.js
@@ -1,29 +1,26 @@
 /**
  * Determine if a sentence is a pangram.
- * @param  {String} wordToCheck The Word we are checking
+ * @param  {String} wordsToCheck The Word we are checking
  * @return {Boolean} True if the word is a pangram, false if not
  */
-export const isPangram = (wordToCheck) => {
-  let alphabet = "abcdefghijklmnopqrstuvwxyz";
-  let missingLetters = [];
+export const isPangram = (wordsToCheck) => {
+  let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","v","w","x","v","y","v","z"];
   let regex = /\s/g;
-
+  let result = true;
   try {
-    let lowerCaseNoSpaces = wordToCheck.toLowerCase().replace(regex, "");
+    let lowerCaseNoSpaces = wordsToCheck.toLowerCase().replace(regex, "");
 
     alphabet
-      .toLowerCase()
-      .split("")
-      .sort()
-      .forEach((letter) => {
+      .map((letter) => {
         if (lowerCaseNoSpaces.indexOf(letter) === -1) {
-          missingLetters.push(letter);
+          return false;
         }
       });
       
   } catch {
     return false;
   }
-
-  return missingLetters.length >> 0 ? false : true;
+  
+  return result;
+ 
 };
